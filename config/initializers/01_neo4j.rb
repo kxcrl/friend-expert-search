f5fb40a15ad5a6703e47deab3d0511ff506ad6e1
kixcereal <jeramy.couts@gmail.com>
2014-03-15 22:11:17 -0700

    Add neoid

---
+++ config/initializers/01_neo4j.rb
@@ -0,0 +1,24 @@
+require('neography') unless defined?(Neography)
+
+ENV["GDB_URL"] ||= "http://localhost:7474"
+
+uri = URI.parse(ENV["GDB_URL"])
+
+$neo = Neography::Rest.new(uri.to_s)
+
+Neography.configure do |c|
+  c.server = uri.host
+  c.port = uri.port
+
+  if uri.user && uri.password
+    c.authentication = 'basic'
+    c.username = uri.user
+    c.password = uri.password
+  end
+end
+
+Neoid.db = $neo
+
+Neoid.configure do |c|
+  c.enable_subrefs = true
+end
