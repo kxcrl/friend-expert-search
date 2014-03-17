9b4340b7d611255ef14349cb3e8245c8d5d4aed1
kixcereal <jeramy.couts@gmail.com>
2014-03-16 18:23:34 -0700

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,5 +25,25 @@ claudia = Expert.create(
   h3s: ''
 )
 
+linus = Expert.create(
+  name: 'Linus',
+  website: 'http://google.com',
+  website_short: 'http://goog.le',
+  h1s: '',
+  h2s: '',
+  h3s: ''
+)
+
+anna = Expert.create(
+  name: 'Anna',
+  website: 'http://google.com',
+  website_short: 'http://goog.le',
+  h1s: '',
+  h2s: '',
+  h3s: ''
+)
+
 Friendship.create_both(alan, bart)
 Friendship.create_both(claudia, bart)
+Friendship.create_both(linus, bart)
+Friendship.create_both(linus, anna)
